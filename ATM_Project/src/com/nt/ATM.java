3f86085b3f0e2fb8b111a438cbb246598e1bcc1c
PINDIBOYINAPRUDHVI <90148426+PINDIBOYINAPRUDHVI@users.noreply.github.com>
2021-12-06 10:27:19 +0530

    Add files via upload

---
+++ ATM_Project/src/com/nt/ATM.java
@@ -0,0 +1,11 @@
+package com.nt;
+
+import java.io.IOException;
+
+public class ATM extends OptionsMenu {
+	public static void main(String[] args) throws IOException {
+		OptionsMenu optionMenu = new OptionsMenu();
+
+		optionMenu.getLogin();
+	}
+}
